201e8e67be1808c381dddf1997ded380373ee469
Andy Cohen <yourstruly@outlawandy.com>
2015-07-08 05:55:30 -0500

    recompile after pull-request. update demo to use angular 1.4

--- relativeDate.js
+++ relativeDate.js
@@ -26,7 +26,7 @@
           diff = ((new Date()).getTime() - date.getTime()) / 1000;
           day_diff = Math.floor(diff / 86400);
           if (isNaN(day_diff) || day_diff < 0 || day_diff >= _cutoffDay_) {
-            return dateFilter(time, fallbackFormat(override));
+            return dateFilter(date, fallbackFormat(override));
           }
           return day_diff === 0 && (diff < 60 && "just now" || diff < 120 && "about 1 minute ago" || diff < 3600 && Math.floor(diff / 60) + " minutes ago" || diff < 7200 && "about 1 hour ago" || diff < 86400 && Math.floor(diff / 3600) + " hours ago") || day_diff === 1 && "Yesterday" || day_diff < 7 && day_diff + " days ago" || day_diff === 7 && "a week ago" || Math.ceil(day_diff / 7) + " weeks ago";
         };
